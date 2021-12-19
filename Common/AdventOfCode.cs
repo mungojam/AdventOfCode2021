02d7c5cfe22fd0412665c2067aa12b6e378bb29c
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2021-12-19 14:41:41 +0000

    Day 1

---
+++ Common/AdventOfCode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+
+namespace Common;
+
+public static class AdventOfCode
+{
+    private static readonly string AssemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+    private static readonly string InputsFolder = Path.Combine(AssemblyFolder, "Inputs");
+    
+    public static ImmutableArray<int> LoadInputs(int day) => 
+        File.ReadAllLines(Path.Combine(InputsFolder, $"Day{day}.txt"))
+            .Select(int.Parse)
+            .ToImmutableArray();
+
+    public static void PrintAnswer(object obj)
+    {
+        Console.WriteLine("Answer is:");
+        Console.Write(obj);
+        Console.ReadLine();
+    }
+    
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Common;
+
+var input = AdventOfCode.LoadInputs(1);
+
+var answer = input.Zip(input.Skip(1)).Count(x => x.Second > x.First);
+
+AdventOfCode.PrintAnswer(answer);
