c15d41fc23d55fe01190e7bd55d7d3fabe88de13
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2021-12-21 21:25:58 +0000

    Day 5 part 2

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -8,18 +8,28 @@
     return new Line(point1, point2);
 });
 
-var allPoints = inputs.SelectMany(line => line.AllPointsFromSimpleLines).ToImmutableArray();
+var allPointsSimple = inputs.SelectMany(line => line.AllPointsSimple).ToImmutableArray();
+var allPoints = inputs.SelectMany(line => line.AllPoints).ToImmutableArray();
 
-var pointHits = allPoints
-    .GroupBy(x => x)
-    .Select(x => (point: x.Key, hitCount: x.Count()))
-    .ToImmutableArray();
+var dangerousPoints1 = DangerousPoints(allPointsSimple);
+var dangerousPoints2 = DangerousPoints(allPoints);
 
-var dangerousPoints = pointHits.Where(x => x.hitCount >= 2).ToImmutableArray();
-
-var answer1 = dangerousPoints.Length;
+var answer1 = dangerousPoints1.Length;
+var answer2 = dangerousPoints2.Length;
 
 PrintAnswer(1, answer1);
+PrintAnswer(2, answer2);
+
+static ImmutableArray<(Point point, int hitCount)> DangerousPoints(ImmutableArray<Point> points)
+{
+    var pointHits = points
+        .GroupBy(x => x)
+        .Select(x => (point: x.Key, hitCount: x.Count()))
+        .ToImmutableArray();
+
+    var dangerousPoints = pointHits.Where(x => x.hitCount >= 2).ToImmutableArray();
+    return dangerousPoints;
+}
 
 internal record Point(int X, int Y)
 {
@@ -32,14 +42,28 @@ internal record Point(int X, int Y)
 
 internal record Line(Point Point1, Point Point2)
 {
-    public ImmutableArray<Point> AllPointsFromSimpleLines => (Point1, Point2) switch
+    public ImmutableArray<Point> AllPoints => AllPointsSimple switch
+    {
+        var x when x.Any() => x,
+        _ =>
+            (Point1, Point2) switch
+            {
+                var ((x1, y1), (x2, y2)) when Abs(x2 - x1) == Abs(y2 - y1) => Range(0, Abs(y2 - y1) + 1)
+                    .Select(offset => new Point(x1 + Sign(x2 - x1) * offset, y1 + Sign(y2 - y1) * offset))
+                    .ToImmutableArray(),
+                _ => throw new ApplicationException("Didn't expect such a tricky line")
+            }
+    };
+
+
+    public ImmutableArray<Point> AllPointsSimple => (Point1, Point2) switch
     {
         var ((x1, y1), (x2, y2)) when x1 == x2 => Range(Min(y1, y2), Abs(y2 - y1) + 1)
-                                                        .Select(y => new Point(x1, y))
-                                                        .ToImmutableArray(),
+            .Select(y => new Point(x1, y))
+            .ToImmutableArray(),
         var ((x1, y1), (x2, y2)) when y1 == y2 => Range(Min(x1, x2), Abs(x2 - x1) + 1)
-                                                        .Select(x => new Point(x, y1))
-                                                        .ToImmutableArray(),
+            .Select(x => new Point(x, y1))
+            .ToImmutableArray(),
         _ => ImmutableArray<Point>.Empty
     };
 }
