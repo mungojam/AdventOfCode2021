756fc73be56a5abcf319e6a7f9ff36c203bf86ce
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2021-12-21 21:03:00 +0000

    Day 5 part 1

--- AdventOfCode.cs
+++ AdventOfCode.cs
@@ -1,3 +1,4 @@
 ﻿global using static Common.AdventOfCode;
 global using static System.Linq.Enumerable;
+global using static System.Math;
 global using System.Collections;
---
+++ Day5/Program.cs
@@ -0,0 +1,45 @@
+﻿var inputs = LoadInputs(5, line =>
+{
+    var pointStrings = line.Split(" -> ");
+
+    var point1 = Point.Parse(pointStrings[0]);
+    var point2 = Point.Parse(pointStrings[1]);
+
+    return new Line(point1, point2);
+});
+
+var allPoints = inputs.SelectMany(line => line.AllPointsFromSimpleLines).ToImmutableArray();
+
+var pointHits = allPoints
+    .GroupBy(x => x)
+    .Select(x => (point: x.Key, hitCount: x.Count()))
+    .ToImmutableArray();
+
+var dangerousPoints = pointHits.Where(x => x.hitCount >= 2).ToImmutableArray();
+
+var answer1 = dangerousPoints.Length;
+
+PrintAnswer(1, answer1);
+
+internal record Point(int X, int Y)
+{
+    internal static Point Parse(string pointString)
+    {
+        var values = pointString.Split(',').Select(int.Parse).ToImmutableArray();
+        return new Point(values[0], values[1]);
+    }
+}
+
+internal record Line(Point Point1, Point Point2)
+{
+    public ImmutableArray<Point> AllPointsFromSimpleLines => (Point1, Point2) switch
+    {
+        var ((x1, y1), (x2, y2)) when x1 == x2 => Range(Min(y1, y2), Abs(y2 - y1) + 1)
+                                                        .Select(y => new Point(x1, y))
+                                                        .ToImmutableArray(),
+        var ((x1, y1), (x2, y2)) when y1 == y2 => Range(Min(x1, x2), Abs(x2 - x1) + 1)
+                                                        .Select(x => new Point(x, y1))
+                                                        .ToImmutableArray(),
+        _ => ImmutableArray<Point>.Empty
+    };
+}
