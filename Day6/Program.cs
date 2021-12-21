61729c55dda6199b71e4d43bd25b53d84cce03b9
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2021-12-21 21:39:51 +0000

    Day 6 part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,22 @@
+﻿var startingAges = LoadInputs(
+    6, x=>x.Split(',').Select(int.Parse).ToImmutableArray()
+    ).Single();
+
+var answer1 = GetFinalState(80).Length;
+
+PrintAnswer(1, answer1);
+
+ImmutableArray<int> GetFinalState(int numDays) =>
+    Range(1, numDays)
+        .Aggregate(startingAges, (ages, _) => NextDay(ages));
+
+ImmutableArray<int> NextDay(ImmutableArray<int> ages)
+{
+    var simpleParents = ages.Select(age => age - 1).ToImmutableArray();
+    var childrenCount = simpleParents.Count(x => x == -1);
+    var children = Enumerable.Repeat(8, childrenCount);
+
+    var actualParents = simpleParents.Select(age => age == -1 ? 6 : age);
+
+    return actualParents.Concat(children).ToImmutableArray();
+}
