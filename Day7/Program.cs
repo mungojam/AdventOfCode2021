8ce1e902dc4b3d1bc4a91516cc50e2e09070b035
Mark Adamson <3154635+mungojam@users.noreply.github.com>
2021-12-22 23:17:52 +0000

    Day 7 part 1

---
+++ Day7/Program.cs
@@ -0,0 +1,13 @@
+﻿var horizontalPositions = LoadInputs(
+    7, x => x.Split(',').Select(int.Parse).ToImmutableArray()
+).Single().OrderBy(x => x).ToImmutableArray();
+
+var count = horizontalPositions.Length;
+
+//get the median for the minimum absolute distance, explained well here:
+//https://math.stackexchange.com/a/113336
+var idealHorizontalPosition = horizontalPositions[count / 2];
+
+var fuel = horizontalPositions.Sum(pos => Abs(pos - idealHorizontalPosition));
+
+PrintAnswer(1, fuel);
